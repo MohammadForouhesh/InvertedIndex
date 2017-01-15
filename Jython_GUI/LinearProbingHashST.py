b5733823bab995c86a00216cc31f63611bf78a43
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2017-01-15 09:10:34 +0330

    hashMap linear probing and Separate chaning added but not tested

---
+++ Jython_GUI/LinearProbingHashST.py
@@ -0,0 +1,98 @@
+from collections import MutableMapping
+from random import randrange
+
+
+class ProbeHashMap(MutableMapping):
+    """Hash map implemented with linear probing for collision resolution."""
+    _AVAIL = object()  # sentinal marks locations of previous deletions
+
+    class _Item:
+        """Lightweight composite to store key-value pairs as map items."""
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __eq__(self, other):
+            return self._key == other._key
+
+        def __ne__(self, other):
+            return not (self == other)
+
+        def __lt__(self, other):
+            return self._key < other._key                       # compare items based on their keys
+
+    def __init__(self, cap=11, p=109345121):
+        self._table = cap * [None]
+        self._n = 0
+        self._prime = p
+        self._scale = 1 + randrange(p - 1)
+        self._shift = randrange(p)
+
+    def _hash_function(self, k):
+        return (hash(k) * self._scale + self._shift) % self._prime % len(self._table)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, k):
+        j = self._hash_function(k)
+        return self._bucket_getitem(j, k)
+
+    def __setitem__(self, k, v):
+        j = self._hash_function(k)
+        self._bucket_setitem(j, k, v)
+        if self._n > len(self._table) // 2:                     # keep load factor <= 0.5
+            self._resize(2 * len(self._table) - 1)              # number 2^x - 1 is often prime
+
+    def __delitem__(self, k):
+        j = self._hash_function(k)
+        self._bucket_delitem(j, k)
+        self._n -= 1
+
+    def _resize(self, c):
+        old = list(self.items())
+        self._table = c * [None]
+        self._n = 0
+        for (k, v) in old:
+            self[k] = v
+
+    def _is_available(self, j):
+        return self._table[j] is None or self._table[j] is ProbeHashMap._AVAIL
+
+    def _find_slot(self, j, k):
+        firstAvail = None
+        while True:
+            if self._is_available(j):
+                if firstAvail is None:
+                    firstAvail = j
+                if self._table[j] is None:
+                    return (False, firstAvail)
+            elif k == self._table[j]._key:
+                return (True, j)
+            j = (j + 1) % len(self._table)
+
+    def _bucket_getitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k))
+        return self._table[s]._value
+
+    def _bucket_setitem(self, j, k, v):
+        found, s = self._find_slot(j, k)
+        if not found:
+            self._table[s] = self._Item(k, v)
+            self._n += 1
+        else:
+            self._table[s]._value = v
+
+    def _bucket_delitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k))
+        self._table[s] = ProbeHashMap._AVAIL
+
+    def __iter__(self):
+        for j in range(len(self._table)):
+            if not self._is_available(j):
+                yield self._table[j]._key
---
+++ Jython_GUI/SeparateChainingHashST.py
@@ -0,0 +1,114 @@
+from collections import MutableMapping
+from random import randrange
+
+
+class ChainHashMap(MutableMapping):
+    """Hash map implemented with separate chaining for collision resolution."""
+    class _Item:
+        """store key-value pairs."""
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __eq__(self, other):
+            return self._key == other._key
+
+        def __ne__(self, other):
+            return not (self == other)
+
+        def __lt__(self, other):
+            return self._key < other._key                   # compare items based on their keys
+
+    def __init__(self, cap=11, p=109345121):
+        self._table = cap * [None]
+        self._n = 0
+        self._prime = p
+        self._scale = 1 + randrange(p - 1)
+        self._shift = randrange(p)
+
+    def _hash_function(self, k):
+        return (hash(k) * self._scale + self._shift) % self._prime % len(self._table)
+
+    def __len__(self):
+        return self._n
+
+    def __getitem__(self, k):
+        j = self._hash_function(k)
+        return self._bucket_getitem(j, k)
+
+    def __setitem__(self, k, v):
+        j = self._hash_function(k)
+        self._bucket_setitem(j, k, v)
+        if self._n > len(self._table) // 2:                     # keep load factor <= 0.5
+            self._resize(2 * len(self._table) - 1)              # number 2^x - 1 is often prime
+
+    def __delitem__(self, k):
+        j = self._hash_function(k)
+        self._bucket_delitem(j, k)
+        self._n -= 1
+
+    def _resize(self, c):
+        old = list(self.items())
+        self._table = c * [None]
+        self._n = 0
+        for (k, v) in old:
+            self[k] = v
+
+    def _bucket_getitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))
+        return bucket[k]
+
+    def _bucket_setitem(self, j, k, v):
+        if self._table[j] is None:
+            self._table[j] = UnsortedTableMap()
+        oldsize = len(self._table[j])
+        self._table[j][k] = v
+        if len(self._table[j]) > oldsize:
+            self._n += 1
+
+    def _bucket_delitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))
+        del bucket[k]
+
+    def __iter__(self):
+        for bucket in self._table:
+            if bucket is not None:
+                for key in bucket:
+                    yield key
+
+
+class UnsortedTableMap(MutableMapping):
+    def __init__(self):
+        self._table = []
+
+    def __getitem__(self, k):
+        for item in self._table:
+            if k == item._key:
+                return item._value
+        raise KeyError('Key Error: ' + repr(k))
+
+    def __setitem__(self, k, v):
+        for item in self._table:
+            if k == item._key:
+                item._value = v
+                return
+        self._table.append(self._Item(k, v))
+
+    def __delitem__(self, k):
+        for j in range(len(self._table)):
+            if k == self._table[j]._key:
+                self._table.pop(j)
+                return  # and quit
+        raise KeyError('Key Error: ' + repr(k))
+
+    def __len__(self):
+        return len(self._table)
+
+    def __iter__(self):
+        for item in self._table:
+            yield item._key                                     # yield the KEY
